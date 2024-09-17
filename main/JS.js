3ddfaad4cb4b15ae30de94b4651b7eb718db7bd5
Oliver Greis Andersen <98947339+OliverGreis@users.noreply.github.com>
2024-09-17 19:03:37 +0200

    Add files via upload

---
+++ main/JS.js
@@ -0,0 +1,11 @@
+
+let input = "";
+
+
+function myFunction(){
+    console.log(2);
+    input = document.getElementById("text").value;
+    if(input == "adgangskode"){
+        window.location.href = "HomePage.html";
+    }
+}
